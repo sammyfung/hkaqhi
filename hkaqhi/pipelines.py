287ad6e3d759e4df683018dd6a8285acbe0b27e9
Sammy Fung <sammy@sammy.hk>
2014-05-11 02:28:16 +0800

    combile AQHI data and Pollutant data into one single row per station report, and making AQHI/Pollutant as optional fields (blank is True) at Django

--- hkaqhi/pipelines.py
+++ hkaqhi/pipelines.py
@@ -9,12 +9,48 @@ from airquality.models import AirQuality
 class HkaqhiPipeline(object):
     def process_item(self, item, spider):
       if spider.name == 'pollutant24':
-        if not AirQuality.objects.filter(reptime = item['reptime'], stationid=item['stationid'], aqhi__isnull = True):
+        if AirQuality.objects.filter(reptime = item['reptime'], stationid=item['stationid'], stationtype__isnull = True):
+          obj = AirQuality.objects.filter(reptime = item['reptime'], stationid=item['stationid'])
+          try:
+            obj.update(no2=item['no2'])
+          except KeyError:
+            pass
+          try:
+            obj.update(o3=item['o3'])
+          except KeyError:
+            pass
+          try:
+            obj.update(so2=item['so2'])
+          except KeyError:
+            pass
+          try:
+            obj.update(co=item['co'])
+          except KeyError:
+            pass
+          try:
+            obj.update(pm10=item['pm10'])
+          except KeyError:
+            pass
+          try:
+            obj.update(pm25=item['pm25'])
+          except KeyError:
+            pass
+        elif not AirQuality.objects.filter(reptime = item['reptime'], stationid=item['stationid']):
           item.save()
         else:
           raise DropItem("Pollutant data time %s from station %s exists." % (item['reptime'],item['name']))
       elif spider.name == 'aqhi24':
-        if not AirQuality.objects.filter(reptime = item['reptime'], stationid=item['stationid'], aqhi__isnull = False):
+        if AirQuality.objects.filter(reptime = item['reptime'], stationid=item['stationid'], aqhi__isnull = True):
+          obj = AirQuality.objects.filter(reptime = item['reptime'], stationid=item['stationid'])
+          try:
+            obj.update(stationtype=item['stationtype'])
+          except KeyError:
+            pass
+          try:
+            obj.update(aqhi=item['aqhi'])
+          except KeyError:
+            pass
+        elif not AirQuality.objects.filter(reptime = item['reptime'], stationid=item['stationid']):
           item.save()
         else:
           raise DropItem("AQHI data time %s from station %s exists." % (item['reptime'],item['name']))
