199ab8d3820301ecb18a11c711dc2ca4c360f330
Sammy Fung <sammy@sammy.hk>
2014-05-22 21:42:13 +0800

    added hourly data query for json result

--- airqdata/airquality/views.py
+++ airqdata/airquality/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from django.http import HttpResponse
+from airquality.models import AirQuality
+from datetime import datetime
+import json
+
+def hourly_data(request):
+  latest_time = request.GET.get('time', 0)
+  json_data = {}
+  if latest_time == 0:
+    latest_time = AirQuality.objects.order_by('-reptime')[0].reptime.__str__()
+  for station in AirQuality.objects.values().filter(reptime = latest_time):
+    json_data[station['stationcode']] = station
+    json_data[station['stationcode']]['reptime'] = station['reptime'].__str__()
+  return HttpResponse(json.dumps(json_data), content_type="application/json")
