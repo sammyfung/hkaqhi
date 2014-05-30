f4bfdbea74665c47f5bc6ea1000fc45cfd27229e
Sammy Fung <sammy@sammy.hk>
2014-05-30 15:03:50 +0800

    json hourly data return HKT time insteads of UTC

--- airqdata/airquality/views.py
+++ airqdata/airquality/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from airquality.models import AirQuality
 from datetime import datetime
-import json
+import json, pytz
 
 def hourly_data(request):
   latest_time = request.GET.get('time', 0)
@@ -10,5 +10,6 @@ def hourly_data(request):
     latest_time = AirQuality.objects.order_by('-reptime')[0].reptime.__str__()
   for station in AirQuality.objects.values().filter(reptime = latest_time):
     json_data[station['stationcode']] = station
-    json_data[station['stationcode']]['reptime'] = station['reptime'].__str__()
+    hkt = pytz.timezone('Asia/Hong_Kong')
+    json_data[station['stationcode']]['reptime'] = station['reptime'].astimezone(hkt).__str__()
   return HttpResponse(json.dumps(json_data), content_type="application/json")
